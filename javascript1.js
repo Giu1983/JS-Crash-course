fbbcc73e820045738eeaa8d08fe3f30491995271
Giu1983 <42543829+Giu1983@users.noreply.github.com>
2018-10-17 10:12:01 +0200

    Add files via upload

---
+++ javascript1.js
@@ -0,0 +1,144 @@
+javascript.js
+//Geolocalization
+//  Agenda –To do  list App
+
+
+// classes: Hairstylist, Beautyshop, Users
+
+
+
+
+// This project is an agenda-to do list app with geolocalization.
+
+
+// For now, the following interactions are:
+
+/ For now, the following interactions are:
+
+  //Hairstylist – Beautyshop (with name, guests, city, comments)
+                   //Weekly or montly service – offer (date, number, price)
+                  //Customer (name & comments). 
+
+             Var Hairstylist = class {
+                 Const (name, guest, city, comments) {
+                     This.name = name
+                     This.guest = guest
+                     This.city = city
+                     This. Comments = []
+                       }
+
+                 addComment() {
+                  this.comments.push(comment)
+}
+}
+              addGuest(guest){
+               this.guest.push(guest)
+}
+            
+
+              addCity(city) {
+             this.cities.push(city)
+}
+
+//Create an Offer class
+
+
+
+
+var Offer = class {
+
+  const(date, number, price) {
+
+    this.date = date
+
+    this.number = number
+
+    this.price = price
+
+  }
+
+
+
+
+  sayUserName() {
+
+    console.log('My name is', this.name)
+
+  }
+
+
+
+
+  sayShopPlace() {
+
+    console.log('The shop is', this.place)
+
+  }
+
+
+
+
+  Set Price (offer) {
+
+    console.log('It cost EUR', this.price)
+
+    offer.price = this.price
+
+    
+
+  }
+
+}
+
+
+
+
+Var haistylist1 = new Seller('hairstylist1', 'Berlin')
+
+var hairstylist2 = new Seller('haistylist2', 'Leipzig')
+
+var haistylist3 = new Seller('haistylist3', 'Dusserldorf')
+
+
+Var beautyshop1 = new Seller('beautyshop1', 'Berlin')
+
+var beautyshop2 = new Seller('beautyshop2', 'Leipzig')
+
+var beautyshop3 = new Seller('beautyshop3', 'Dusserldorf')
+
+
+Hairstylist1.sayPlace()
+
+ Hairstylist1.set_offer-service(x)
+
+ Beautyshop1.sayName()
+
+ Beautyshop1.sayPlace()
+
+ Beautyshop1.set_offer_service(y)
+
+
+
+// check that everything got recorded correctly
+                             //Shop
+ Console.log(hairstylist1)
+   Console.log(hairstylist2)
+   Console.log(hairstylist3)
+   Console.log(beautyshop1)
+   Console.log(beautyshop2)
+   Console.log(beautyshop3)
+
+
+//Offer-service
+
+Console.log(x)
+Console.log(y)
+
+
+//User
+Console.log(A)
+Console.log(B)
+Console.log(C)
+            
+
+
